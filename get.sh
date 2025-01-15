d7bf52f9c2b6096ff0743460ba90e9179f858534
Vamsi-PrimeSoft <133194079+Vamsi-PrimeSoft@users.noreply.github.com>
2025-01-15 18:12:26 +0530

    Update get.sh gh

--- get.sh
+++ get.sh
@@ -6,10 +6,28 @@ SFTP_USER="ubuntu"
 SFTP_HOST="172.16.7.120"
 SFTP_REMOTE_PATH="/home/ubuntu"
 
-# Execute the SFTP command
+# Get the latest .zip file from the remote server
+LATEST_FILE=$(sftp -o StrictHostKeyChecking=no -i "${LOCAL_DOWNLOAD_PATH}/.ssh/id_rsa" "${SFTP_USER}@${SFTP_HOST}" <<EOF | grep -oE '^[^ ]+\.zip$' | head -n 1
+cd "${SFTP_REMOTE_PATH}"
+ls -t *.zip
+bye
+EOF
+)
+
+# Check if LATEST_FILE is not empty
+if [ -z "$LATEST_FILE" ]; then
+  echo "No .zip files found in the remote directory."
+  exit 1
+fi
+
+echo "Latest file: $LATEST_FILE"
+
+# Download the latest .zip file
 sftp -o StrictHostKeyChecking=no -i "${LOCAL_DOWNLOAD_PATH}/.ssh/id_rsa" "${SFTP_USER}@${SFTP_HOST}" <<EOF
 cd "${SFTP_REMOTE_PATH}"
 lcd "${LOCAL_DOWNLOAD_PATH}"
-get \$(ls -t *.zip | head -n 1)
+get "${LATEST_FILE}"
 bye
 EOF
+
+echo "Download complete."
